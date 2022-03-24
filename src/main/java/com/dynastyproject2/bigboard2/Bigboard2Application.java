5c5c89a89291d59eefaa08a37a90171502291a7e
Ryan Jones <rkjonesvo@gmail.com>
2022-03-23 23:42:41 -0500

    added 'dataSourceAutoConfiguration' exclusion to application.java class

--- src/main/java/com/dynastyproject2/bigboard2/Bigboard2Application.java
+++ src/main/java/com/dynastyproject2/bigboard2/Bigboard2Application.java
@@ -1,7 +1,9 @@
 package com.dynastyproject2.bigboard2;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -15,6 +17,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 @EnableJpaAuditing
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class Bigboard2Application {
 
 	public static void main(String[] args) {
